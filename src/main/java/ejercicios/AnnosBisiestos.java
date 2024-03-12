266450a01a747bb79b4df88bf162c2749edb2d33
LopezDEL <diegoestebanlopezrico3@gmail.com>
2024-03-12 18:03:06 -0500

    AÑOSBISIESTOS

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -1,7 +1,5 @@
-
 package ejercicios;
 
-import static ejercicios.SetDeTenis.evaluar;
 import java.util.Scanner;
 
 /**
@@ -10,8 +8,11 @@ import java.util.Scanner;
  */
 public class AnnosBisiestos {
     public static String evaluar(int anno){
-        // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-        return "";
+        if ((anno % 4 == 0 && anno % 100 != 0) || anno % 400 == 0) {
+            return anno + " es bisiesto";
+        } else {
+            return anno + " no es bisiesto";
+        }
     }
     
     public static void main(String[] args) {
@@ -22,5 +23,4 @@ public class AnnosBisiestos {
         String respuesta = evaluar(anno);
         System.out.println(respuesta);
     }
-            
 }
